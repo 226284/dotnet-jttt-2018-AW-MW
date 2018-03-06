1c047e628625fdc35055b98a5df6d308951c5461
Amadeusz Wach <amadeusz.wach@scz.pl>
2018-03-06 03:39:37 +0100

    Tworzenie wiadomosci Wysylanie wiadomosci

--- JTTT/JTTT/MainWindow.xaml.cs
+++ JTTT/JTTT/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using HtmlAgilityPack;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 
 namespace JTTT
 {
@@ -24,6 +27,11 @@ namespace JTTT
     {
         private string url;
         private string reply;
+
+        private string fromMail = "JTTT";
+        private string fromMailAdress = "amadi@scz.pl";
+
+        private string toMailAdress;
         
         public MainWindow()
         {
@@ -36,11 +44,26 @@ namespace JTTT
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs ev)
         {
             
             var URLBox = (TextBox)this.FindName("URL");
             var KeyBox = (TextBox)this.FindName("Key");
+            var MailBox = (TextBox)this.FindName("Mail");
 
             var ErrorBlock = (TextBlock)this.FindName("error");
 
@@ -53,6 +76,66 @@ namespace JTTT
 
                 WebClient client = new WebClient();
                 reply = client.DownloadString(url);
+
+                if (IsValidEmail(MailBox.Text))
+                {
+                    toMailAdress = MailBox.Text;
+
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress(fromMail, fromMailAdress));
+                    message.To.Add(new MailboxAddress("YourName", toMailAdress));
+                    message.Subject = "Key found event!";
+
+                    // create our message text, just like before (except don't set it as the message.Body)
+                    var body = new TextPart("plain")
+                    {
+                        Text = @"Hey YourName,
+
+We found your key("+KeyBox.Text+") on site "+url+@"
+
+Have nice day!
+
+-- JTTT
+"
+                    };
+
+                    // create an image attachment for the file located at path
+                   /* var attachment = new MimePart("image", "gif")
+                    {
+                        Content = new MimeContent(File.OpenRead(path), ContentEncoding.Default),
+                        ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                        ContentTransferEncoding = ContentEncoding.Base64,
+                        FileName = Path.GetFileName(path)
+                    };*/
+
+                    // now create the multipart/mixed container to hold the message text and the
+                    // image attachment
+                    var multipart = new Multipart("mixed");
+                    multipart.Add(body);
+                   // multipart.Add(attachment);
+
+                    // now set the multipart/mixed as the message body
+                    message.Body = multipart;
+
+                    using (var mailClient = new SmtpClient())
+                    {
+                        mailClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                        mailClient.Connect("scz.pl", 587, SecureSocketOptions.Auto);
+
+                        ////Note: only needed if the SMTP server requires authentication
+                        mailClient.Authenticate(fromMailAdress, "Amadeusz");
+
+                        mailClient.Send(message);
+                        mailClient.Disconnect(true);
+                    }
+
+                } else
+                {
+                    ErrorBlock.Text = "Check Email";
+                    ErrorBlock.Visibility = Visibility.Visible;
+                }
+
             }
             else
             {
