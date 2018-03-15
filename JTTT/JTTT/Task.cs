1aadd6052c114ba96523f8a9ed34ff9077d7da23
Michał Wieczorek <michal126@hotmail.com>
2018-03-15 16:08:17 +0100

    Task and List of task added

---
+++ JTTT/JTTT/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTTT
+{
+    class Task
+    {
+        protected Action Action;
+        protected Condition Condition;
+        protected Log Log;
+
+        public Task(Action action, Condition condition, Log log)
+        {
+            Log = log;
+            Action = action;
+            Condition = condition;
+        }
+    }
+}
